587e09042fdfb0e9c5251f900a7293221c65220f
Morichetti Elian <morichettielian@hotmail.com>
2024-12-22 18:34:39 +0000

    fix: Finished Guessing Game

--- number_guess.sh
+++ number_guess.sh
@@ -6,11 +6,17 @@ SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 
 GUESS_NUMBER(){
   
+if [[ $1 ]]
+then
+  USERNAME_SELECTED=$1
+  IFS='|' read USERNAME GAMES_PLAYED BEST_GAME <<< "$USERNAME_SELECTED"
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
   echo -e "\nGuess the secret number between 1 and 1000:"
    
-  NUMBER_OF_GUESSES=0
-
   read NUMBER_ENTERED
+  NUMBER_OF_GUESSES=1
   
   while [[ $NUMBER_ENTERED != $SECRET_NUMBER ]]
   do
@@ -18,7 +24,6 @@ GUESS_NUMBER(){
    if [[ ! $NUMBER_ENTERED =~ ^[0-9]+$ ]]
     then
      echo -e "\nThat is not an integer, guess again:"
-     sleep 1
      read NUMBER_ENTERED
     else
      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
@@ -35,11 +40,11 @@ GUESS_NUMBER(){
   done
 
   echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-  
+
   GAMES_UPDATED=$((GAMES_PLAYED + 1))
 
   # fewest number of guesses it took that user to win the game
-  if [[ $NUMBER_OF_GUESSES < $BESTGAME || -z $BESTGAME ]]
+  if [[ $NUMBER_OF_GUESSES < $BEST_GAME || -z $BEST_GAME ]]
   then 
     UPDATE=$($PSQL "UPDATE users SET games_played = $GAMES_UPDATED, best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME'")
   else
@@ -51,24 +56,18 @@ GUESS_NUMBER(){
 echo -e "\nEnter your username:"
 read USERNAME_INPUT
 
-USERNAME=$($PSQL "SELECT username,games_played,best_game FROM users WHERE username = '$USERNAME_INPUT'")
+USERNAME_SELECTED=$($PSQL "SELECT username,games_played,best_game FROM users WHERE username = '$USERNAME_INPUT'")
 
-if [[ -z $USERNAME ]]
+if [[ -z $USERNAME_SELECTED ]]
 then
 
   USERNAME=$USERNAME_INPUT
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   
-  INSERT=$($PSQL "INSERT INTO users(username,games_played,best_game) VALUES('$USERNAME',0,NULL)") # MOVE TO THE END
-  sleep 2
+  INSERT=$($PSQL "INSERT INTO users(username,games_played,best_game) VALUES('$USERNAME',0,NULL)")
   GUESS_NUMBER
 
 else
-  echo $USERNAME | while IFS='|' read USERNAME GAMES_PLAYED BESTGAME
-  do
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BESTGAME guesses."
-    sleep 5
-    GUESS_NUMBER
-  done
+  GUESS_NUMBER $USERNAME_SELECTED
 fi
 
